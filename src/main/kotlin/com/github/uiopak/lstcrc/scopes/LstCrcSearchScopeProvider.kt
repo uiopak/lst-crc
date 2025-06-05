fcdfcffee20a6bec8e81ef4230c63308e5294da8
uiopak <44174807+uiopak@users.noreply.github.com>
2025-06-05 23:27:12 +0200

    Refactor scope management by replacing `LstCrcScopeDescriptorProvider` with `LstCrcSearchScopeProvider` and updating `plugin.xml` accordingly.

---
+++ src/main/kotlin/com/github/uiopak/lstcrc/scopes/LstCrcSearchScopeProvider.kt
@@ -0,0 +1,37 @@
+package com.github.uiopak.lstcrc.scopes
+
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.project.Project
+import com.intellij.psi.search.SearchScope
+import com.intellij.psi.search.SearchScopeProvider
+import com.intellij.psi.search.scope.packageSet.NamedScopeManager
+
+/**
+ * Provides the LSTCRC scopes under a "LSTCRC Changes" group in the scope selection dropdown.
+ * This replaces the need for a ScopeDescriptorProvider.
+ */
+class LstCrcSearchScopeProvider : SearchScopeProvider {
+
+    override fun getDisplayName(): String {
+        // This string will be used as a separator/group header in the scope selection dropdown.
+        return "LSTCRC Changes"
+    }
+
+    override fun getSearchScopes(project: Project, dataContext: DataContext): List<SearchScope> {
+        val namedScopeManager = NamedScopeManager.getInstance(project)
+
+        // We get the singleton instances of our scopes from LstCrcScopeProvider.
+        val createdScope = LstCrcScopeProvider.CREATED_FILES_SCOPE
+        val modifiedScope = LstCrcScopeProvider.MODIFIED_FILES_SCOPE
+        val movedScope = LstCrcScopeProvider.MOVED_FILES_SCOPE
+        val changedScope = LstCrcScopeProvider.CHANGED_FILES_SCOPE
+
+        // Each NamedScope is wrapped in our custom SearchScope implementation.
+        return listOf(
+            NamedScopeWrapper(project, createdScope, namedScopeManager),
+            NamedScopeWrapper(project, modifiedScope, namedScopeManager),
+            NamedScopeWrapper(project, movedScope, namedScopeManager),
+            NamedScopeWrapper(project, changedScope, namedScopeManager)
+        )
+    }
+}
